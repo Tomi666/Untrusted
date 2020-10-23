00c3bb77321a1809945dfdfda261068a87fa903a
Tomi666 <tamas.tisoczki@gmail.com>
2020-10-23 19:41:20 +0200

    Level 12 done

--- solutions.js
+++ solutions.js
@@ -79,6 +79,21 @@
         //             }
 
 // ***** Level 12 - robotNav *****
+    // I actually couldn't do this one without the solution :(
+        //           	me.path = me.path || [];
+        //             if (me.path.length) {
+        //             	me.move(me.path.pop());
+        // 	        } else if (me.canMove('down')) {
+        //                 me.move('down');
+        //             } else if (me.canMove('right')) {
+        //                 me.move('right');
+        //             } else {
+        //             	me.path = [
+        //      	  			'right', 'right', 'right',
+        //                     'up', 'up', 'up', 'up', 'up',
+        //                     'left', 'left', 'left', 'left'
+        //                 ];
+        //             }
 
 // ***** Level 13 - robotMaze *****
 
